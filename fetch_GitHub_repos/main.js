3220bf54f5daf0f282a4b125072ddf6d9729853d
moustafa-greeda <moustafagreeda@gmail.com>
2024-06-30 15:02:35 +0300

    first commit

--- fetch_GitHub_repos/main.js
+++ fetch_GitHub_repos/main.js
@@ -59,6 +59,6 @@ function getRepoLocalStorage() {
 
 
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json));
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json));
